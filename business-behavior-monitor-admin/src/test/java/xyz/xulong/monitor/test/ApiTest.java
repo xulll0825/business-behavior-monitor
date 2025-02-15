f924a8b045ecbbff3a01d5ac52114a16a4673ffd
xulll0825 <61831079+xulll0825@users.noreply.github.com>
2025-02-15 11:23:43 +0800

    feat:init project

---
+++ business-behavior-monitor-admin/src/test/java/xyz/xulong/monitor/test/ApiTest.java
@@ -0,0 +1,12 @@
+package xyz.xulong.monitor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ApiTest {
+    @Test
+    public void test_ognl() {
+
+    }
+}
